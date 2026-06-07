f2d27298947ee2195b0d560fa8cc362632a224f1
juncheng <working_fang@163.com>
2026-06-07 10:07:38 +0000

    feat(bit-string): add formatting and borrowed iteration

--- src/bit_string.rs
+++ src/bit_string.rs
@@ -2,6 +2,7 @@ use alloc::boxed::Box;
 
 const WORD_BITS: usize = u64::BITS as usize;
 
+#[derive(Clone, PartialEq, Eq, Hash)]
 pub struct BitString {
     bits: Box<[u64]>,
     len: usize,
@@ -12,5 +13,7 @@ mod impls_for_access;
 mod impls_for_bit_ops;
 mod impls_for_construction;
 mod impls_for_editing;
+mod impls_for_fmt;
+mod impls_for_iter;
 
 pub(crate) mod funcs_for_share;
