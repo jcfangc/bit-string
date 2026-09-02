c84ce03db1c5661df6fe98a9b2ac02f1ab1ae075
juncheng <working_fang@163.com>
2026-09-02 14:11:39 +0000

    validate packed string bit payloads

--- src/packed_string/impls_for_construction/impls_for_from_bits.rs
+++ src/packed_string/impls_for_construction/impls_for_from_bits.rs
@@ -5,15 +5,23 @@ where
     C: PackedChar<BITS>,
 {
     /// Validates and adopts an already packed bit payload.
-    pub fn from_bits(_bits: BitString, _char_len: usize) -> Option<Self> {
-        unimplemented!("PackedString::from_bits")
+    pub fn from_bits(bits: BitString) -> Option<Self> {
+        let () = Self::VALID_WIDTH;
+        let bits_per_char = usize::from(BITS);
+        if bits.bit_len() % bits_per_char != 0 {
+            return None;
+        }
+        let code_mask = u64::from(code_mask::<BITS>());
+        for start in (0..bits.bit_len()).step_by(bits_per_char) {
+            C::from_code((bits.get_chunk(start) & code_mask) as u8)?;
+        }
+        Some(Self {
+            bits,
+            marker: PhantomData,
+        })
     }
 
     pub fn into_bits(self) -> BitString {
         unimplemented!("PackedString::into_bits")
-    }
-
-    pub fn into_parts(self) -> (BitString, usize) {
-        unimplemented!("PackedString::into_parts")
     }
 }
